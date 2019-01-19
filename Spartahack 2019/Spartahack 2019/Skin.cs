1f8fc76790eecf71b6f73980bfcdbd342fca1490
Sebastian Perry <sperry@biggby.com>
2019-01-19 13:35:58 -0500

    Added basic Skin class

---
+++ Spartahack 2019/Spartahack 2019/Skin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Spartahack_2019
+{
+    public class Skin
+    {
+        private Texture2D spriteSheet;
+        private Rectangle source;
+
+        public Skin()
+        {
+        }
+
+        public Texture2D SpriteSheet
+        {
+            get { return spriteSheet; }
+            set { spriteSheet = value; }
+        }
+
+        public Rectangle Source
+        {
+            get { return source; }
+            set { source = value; }
+        }
+    }
+}
