23b33f2b346677e1dddd96f6522c18bb6008e128
carlos gonzalez <carlosg266@gmail.com>
2019-01-19 14:23:43 -0500

    added object class

--- Spartahack 2019/Spartahack 2019/Object.cs
+++ Spartahack 2019/Spartahack 2019/Object.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Spartahack_2019
 {
-    class Class1
+    public class Object
     {
+        private Rectangle bounds;
+        public Object()
+            {
+            }
+        public Rectangle Bounds
+        {
+            get { return bounds; }
+            set { Bounds = value; }
+        }
     }
 }
