cd566d386e6e6c92492260d7f9c8f7577524c931
Sebastian Perry <sperry@biggby.com>
2019-01-19 14:23:24 -0500

    Added basic Level class

---
+++ Spartahack 2019/Spartahack 2019/Level.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Spartahack_2019
+{
+    public class Level
+    {
+        public Level()
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
