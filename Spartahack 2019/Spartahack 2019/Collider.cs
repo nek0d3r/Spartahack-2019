d0655da99394965612a49dac853e3680694e95d2
Sebastian Perry <sperry@biggby.com>
2019-01-19 11:49:41 -0500

    Added basic AABB collider

---
+++ Spartahack 2019/Spartahack 2019/Collider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Spartahack_2019
+{
+    public static class Collider
+    {
+        public static bool AABBTestCollide(Rectangle object1, Rectangle object2)
+        {
+            if (object1.Intersects(object2))
+                return true;
+            return false;
+        }
+    }
+}
