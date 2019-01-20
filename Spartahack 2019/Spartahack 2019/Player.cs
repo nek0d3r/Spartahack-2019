6e140cad3747a82ff49f5189d4c5fb3fbf11ab50
Sebastian Perry <sperry@biggby.com>
2019-01-19 22:05:04 -0500

    Added ghetto collisions

--- Spartahack 2019/Spartahack 2019/Player.cs
+++ Spartahack 2019/Spartahack 2019/Player.cs
@@ -10,6 +10,8 @@ namespace Spartahack_2019
     public class Player : Object
 
     {
+        KeyboardState state;
+        KeyboardState prevState;
         public Vector2 acceleration;
         public Vector2 friction;
         public Vector2 velocity = Vector2.Zero;
@@ -18,6 +20,7 @@ namespace Spartahack_2019
         float maxVelocity = 3.0f;
         float frictionRate = 10.0f;
         float gravity = 0.0f;
+        float maxGravity = 5.0f;
         float initialJumpVelocity = 100.0f;
 
         public Player(Rectangle boundingBox) : base(boundingBox)
@@ -26,9 +29,11 @@ namespace Spartahack_2019
 
         public void Update(GameTime gameTime)
         {
+            prevState = state;
+
             direction = Vector2.Zero;
 
-            KeyboardState state = Keyboard.GetState();
+            state = Keyboard.GetState();
 
             if (state.IsKeyDown(Keys.Right))
                 direction.X = 1;
@@ -36,7 +41,7 @@ namespace Spartahack_2019
             if (state.IsKeyDown(Keys.Left))
                 direction.X = -1;
 
-            if (state.IsKeyDown(Keys.Space))
+            if (state.IsKeyDown(Keys.Space) && prevState.IsKeyUp(Keys.Space))
             {
                 direction.Y = -1;
                 gravity = 15.0f;
@@ -60,10 +65,15 @@ namespace Spartahack_2019
 
             velocity.X += friction.X * time;
             velocity.Y += gravity * time;
+            if (velocity.Y > maxGravity)
+                velocity.Y = maxGravity;
 
             bounds.Offset(velocity);
-
-
+            
+            if (Bounds.Y > Globals.TILE_DIMS.Y * Globals.SPR_DIMS.Y - Bounds.Height)
+            {
+                Bounds = new Rectangle(Bounds.X, Globals.TILE_DIMS.Y * Globals.SPR_DIMS.Y - Bounds.Height, Bounds.Width, Bounds.Height);
+            }
         }
     }
 }
