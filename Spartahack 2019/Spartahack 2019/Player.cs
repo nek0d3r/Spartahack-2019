7619598f035a63b86520b2b4d2db9ef0f71a9952
carlos gonzalez <carlosg266@gmail.com>
2019-01-19 20:29:39 -0500

    player controls

--- Spartahack 2019/Spartahack 2019/Player.cs
+++ Spartahack 2019/Spartahack 2019/Player.cs
@@ -8,31 +8,35 @@ using Microsoft.Xna.Framework;
 namespace Spartahack_2019
 {
     public class Player : Object
+
     {
+        Vector2 acceleration;
+        Vector2 velocity = Vector2.Zero;
+        float accelRate = .3f;
+
         public Player(Rectangle boundingBox) : base(boundingBox)
         {
         }
 
         public void Update(GameTime gameTime)
         {
-            Vector2 acceleration;
-            Vector2 velocity = Vector2.Zero;
+
             Vector2 direction = Vector2.Zero;
-            float accelRate = 100.0f;
 
             KeyboardState state = Keyboard.GetState();
+
             if (state.IsKeyDown(Keys.Right))
-                direction = new Vector2(1, 0);
+                direction = new Vector2(2, 0);
 
             if (state.IsKeyDown(Keys.Left))
-                direction = new Vector2(-1, 0);
+                direction = new Vector2(-2, 0);
 
             if (state.IsKeyDown(Keys.Space))
-                direction = new Vector2(1, -1);
+                direction = new Vector2(0, -5);
 
-
-            acceleration = direction * accelRate;
-            velocity += acceleration * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float time = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            acceleration = accelRate * direction;
+            velocity += acceleration * time;
 
             bounds.Offset(velocity);
 
